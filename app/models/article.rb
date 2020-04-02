22fde372c948d1d36dd5f62743faea4c50f592d0
Michał Kubiak <michal@michkub.net>
2020-04-02 21:56:45 +0200

    walidacja

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+    validates :tytuł, presence: true, length: {minimum: 6, maximum: 100}
+    validates :opis, presence: true, length: {minimum: 10, maximum: 300}
 end
