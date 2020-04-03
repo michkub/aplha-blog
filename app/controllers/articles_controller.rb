404edaf7430725bbfe19b9923309f285329f9783
Michał Kubiak <michal@michkub.net>
2020-04-03 20:18:10 +0200

    walidacja i edycja artykułów

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,32 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+		end
+
+		def edit
+			@article = Article.find(params[:id])
+		end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:tytuł, :opis))
+        if @article.save
+            flash[:notice] = "Artykuł został stworzony!"
+            redirect_to @article
+        else
+            render 'new'
+        end
+		end
+
+		def update
+			@article = Article.find(params[:id])
+			if @article.update(params.require(:article).permit(:tytuł, :opis))
+				flash[:notice] = "Artykuł został zaktualizowany!"
+				redirect_to @article
+			else
+				render 'edit'
+			end
+		end
+
 end
