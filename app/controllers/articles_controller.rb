e0f962eca6beec72e98f8f1add4d30f8c43684f2
Michał Kubiak <michal@michkub.net>
2020-04-04 15:20:27 +0200

    cały CRUD i czyszczenie kodu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -10,30 +10,43 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
-		end
+    end
 
-		def edit
-			@article = Article.find(params[:id])
-		end
+    def edit
+    end
 
     def create
-        @article = Article.new(params.require(:article).permit(:tytuł, :opis))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Artykuł został stworzony!"
             redirect_to @article
         else
             render 'new'
         end
-		end
+    end
 
-		def update
-			@article = Article.find(params[:id])
-			if @article.update(params.require(:article).permit(:tytuł, :opis))
-				flash[:notice] = "Artykuł został zaktualizowany!"
-				redirect_to @article
-			else
-				render 'edit'
-			end
-		end
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Artykuł został zaktualizowany!"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:tytuł, :opis)
+    end
 
 end
