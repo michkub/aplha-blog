b58c0c8b672c1a3328fd937bbb165b2c1f2f869e
Michał Kubiak <michal@michkub.net>
2020-04-17 22:39:43 +0200

    Logowanie i wylogowanie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def show
-        @user = User.find(params[:id])
+
         @articles = @user.articles.paginate(page: params[:page], :per_page => 5).order('created_at DESC')
     end
 
@@ -14,15 +16,13 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
 
     end
 
     def update
-        @user =  User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Twój profil został zaaktualizowany!"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Witaj na Alfa Blog #{@user.username}, zostałeś zarejestrowany"
             redirect_to articles_path
         else
@@ -41,6 +42,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
